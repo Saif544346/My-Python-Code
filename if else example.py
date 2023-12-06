1bdc93a2b5f1e2b7bcd2c49c25bea84fceaf7196
Ibadullah Saif <146116526+Saif544346@users.noreply.github.com>
2023-12-06 18:38:45 +0600

    Add files via upload

---
+++ if else example.py
@@ -0,0 +1,11 @@
+age=int(input("Enter your age:"))
+print("Wellcome!!!Your age is:",age)
+if(age<18):
+    print("You can not drive")
+else:
+    print(" Conratualation you Can drive")
+    print(age<18)
+    print(age>18)
+    print(age==18)
+    print(age!=18)
+print("Thank you")
