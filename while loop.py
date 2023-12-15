13ed8de8ca3e3e17d981d65dadd30cf03c9877f3
Ibadullah Saif <146116526+Saif544346@users.noreply.github.com>
2023-12-16 01:13:26 +0600

    Add files via upload

---
+++ while loop.py
@@ -0,0 +1,14 @@
+i=0;
+while(i<5):
+    print(i)
+    i=i+1
+
+    i=(int(input("enter number:")));
+    while(i<=10):
+        print(i,"saif")
+        i=i+1
+
+        i=(int(input("enter number:")));
+        while(i>0):
+            print(i)
+            i=i-1
