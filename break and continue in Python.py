73f990f40ef5d77744eefb5be8ac440eebe1219b
Ibadullah Saif <146116526+Saif544346@users.noreply.github.com>
2023-12-16 02:15:08 +0600

    Add files via upload

---
+++ break and continue in Python.py
@@ -0,0 +1,12 @@
+for i in range(20):
+    print("5x",i+1,"=",5*(i+1))
+    if(i==15-1):
+        break
+print("Thank you")
+
+for i in range(20):
+
+    if(i==15):
+        continue
+    print("5x", i + 1, "=", 5 * (i + 1))
+print("Thank you")
