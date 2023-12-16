036572e1786e5e9e51a4ab3e860f0f05199c8062
Ibadullah Saif <146116526+Saif544346@users.noreply.github.com>
2023-12-16 18:35:40 +0600

    Add files via upload

---
+++ Operations on Tuples in Python Day#25.py
@@ -0,0 +1,11 @@
+cou1=("bangladesh","srilanka","afganistan")
+print(cou1)
+cou2=("india","vitnam","pakistan")
+print(cou2)
+cou3=cou1+cou2
+print(cou3)
+print(len(cou3))
+tp=(32,44,66,567,56,88,676,34)
+t=tp.index(66)
+print(t)
+                                              
