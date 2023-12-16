0cedd08e90bd295ed909d40b1d4393d309b0b7da
Ibadullah Saif <146116526+Saif544346@users.noreply.github.com>
2023-12-16 17:31:37 +0600

    Add files via upload

---
+++ List Methods in Python Day#23.py
@@ -0,0 +1,13 @@
+lis=[3,53,55,223,453,56,6,75,4,3,1,0,7]
+print(lis)
+#print(lis.count(3))
+#lis.append(10)
+#lis.sort()
+#lis.reverse()
+#m=lis.copy()
+#lis.insert(1,900)
+a=[65,45,78,44,34]
+k=a+lis
+print(k)
+
+print(lis)
