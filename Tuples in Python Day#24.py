6334848ab0cd03251e2b484ec2e8e649e775c112
Ibadullah Saif <146116526+Saif544346@users.noreply.github.com>
2023-12-16 18:12:11 +0600

    Add files via upload

---
+++ Tuples in Python Day#24.py
@@ -0,0 +1,13 @@
+tup=(1,11,4,4,33,24,"saif",True)
+#tup[4]=100
+print(tup)
+print(len(tup))
+print(tup[3])
+print((tup[4]))
+print(tup[-4])
+if 24 in tup:
+    print("yes")
+else:
+    print("no")
+tup2=tup[2:6:2]
+print(tup2)
