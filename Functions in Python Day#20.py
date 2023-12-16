96a17f9b21bf32db631e4cd6891b22e2c89f4b4c
Ibadullah Saif <146116526+Saif544346@users.noreply.github.com>
2023-12-16 13:09:40 +0600

    Add files via upload

---
+++ Functions in Python Day#20.py
@@ -0,0 +1,20 @@
+a=(int(input("Enter first number:")))
+b=(int(input("Enter second number:")))
+def sumofnumbers(a,b):
+
+    sum=a+b
+    print(sum)
+
+sumofnumbers(a,b)
+sumofnumbers(8,9)
+def gratter (a,b):
+    if(a>b):
+        print(a)
+    else:
+        print(b)
+
+gratter(20,56)
+gratter(55,8)
+
+def less(a,b):
+    pass
