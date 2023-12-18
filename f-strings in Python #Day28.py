30a2a6303e1298037101843271ed17520be22823
Ibadullah Saif <146116526+Saif544346@users.noreply.github.com>
2023-12-19 01:51:07 +0600

    Add files via upload

---
+++ f-strings in Python #Day28.py
@@ -0,0 +1,11 @@
+letter="My name is {1} and i am from {0}"
+name="saif"
+country="bangladesh"
+print(f"My name is {name} and i am from {country}")
+print(letter.format(country,name))
+price=5.345343534
+print(f"ptice is {price:.2f}")
+print(f"price is {price}")
+print(type(f"{2*30}"))
+name="saif"
+print(f"my name is {{saif}}")
