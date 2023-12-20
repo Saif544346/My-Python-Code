cbd3626b5921694467bd3a8ea9ef896cc525e87f
Ibadullah Saif <146116526+Saif544346@users.noreply.github.com>
2023-12-20 10:08:20 +0600

    Add files via upload

---
+++ Dictionary Methods in Python#Day34.py
@@ -0,0 +1,26 @@
+ep1={
+    101:45,
+    102:65,
+    103:77,
+    104:88
+}
+ep2={
+    301:90,
+    304:99,
+    305:121
+}
+print(ep1.pop(101))
+print(ep2.popitem())
+print(ep2.popitem())
+print(ep1)
+print(ep1.clear())
+print(ep2)
+ep1.update(ep2)
+print(ep1)
+empty={}
+print(empty)
+name={
+  "n":  ["x","y","z"]
+}
+print(name)
+
